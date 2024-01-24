bab4afe4b519bc7a98d38cabbc81dbd3f3978008
Chandrakanth0698 <chandrakanth0698@gmail.com>
2024-01-24 11:51:55 -0500

    Add MACD and Signal calculations to DF

--- MACD.py
+++ MACD.py
@@ -6,8 +6,8 @@ Created on Tue Jan 23 13:37:45 2024
 """
 
 import yfinance as yf
-import pandas as pd
-import datetime as dt
+#import pandas as pd
+#import datetime as dt
 
 tickers = ["AAPL","MSFT","GOOG","META","CSCO"]
 ohlcv = {}
@@ -22,7 +22,17 @@ def MACD(DF,fast=12, slow=26, signal_len=9):
     MACD - "Moving average Convergence Divergence"
     MACD(args) return the df with 2 additional colums
     "macd" and "signal"
-    macd is calculated as the difference between simple
+    macd calculation:  MACD Line: (12-day EMA - 26-day EMA) 
+    Signal Line: 9-day EMA of MACD Line
+    MACD Histogram: MACD Line - Signal Line
     """
     df = DF.copy()
-    d
+    df["ma_fast"] = df["Adj Close"].ewm(span=fast, min_periods=fast).mean()
+    df["ma_slow"] = df["Adj Close"].ewm(span=slow, min_periods=slow).mean()
+    df["macd"] = df["ma_fast"] - df["ma_slow"]
+    df["signal"] = df["macd"].ewm(span=signal_len, min_periods=signal_len).mean()
+    return df.loc[:,["macd","signal"]]
+
+for ticker in ohlcv:
+    ohlcv[ticker][["MACD","Signal"]] = MACD(ohlcv[ticker])
+        
